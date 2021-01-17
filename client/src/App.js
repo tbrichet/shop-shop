bbd65c8a0c2508a596ab3da1d20e02e79a6b6df6
T Brichetto <TaraPersonal@MacBook-Air.local>
2021-01-17 14:05:41 -0700

    misc bug fixes redux

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './utils/store';
 
 const client = new ApolloClient({
   request: (operation) => {
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -96,3 +96,5 @@ export const reducer = (state=initialState, action) => {
 export function useProductReducer(initialState) {
   return useReducer(reducer, initialState)
 }
+
+export default reducer;
